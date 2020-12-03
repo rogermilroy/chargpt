15a3587e6025d4762324ea4a6041fc901decdba2
Roger Milroy <rogermilroy@icloud.com>
2020-12-03 12:52:35 +0000

    Rename balanced_dataloader.py to balanced_data_sampler.py. Change from Dataloader approach to Sampler approach. Add balanced_dataset.py with a Decorator class for the MNIST datasest specifically.

---
+++ balanced-sampler/balanced-sampler/src/data/balanced_data_sampler.py
@@ -0,0 +1,14 @@
+
+
+class BalancedDataSampler:
+
+    def __init__(self):
+        pass
+
+
+# I  want to use a sampler.
+# but I want to do preprocessing to ensure that the batch size is correct etc.
+
+if __name__ == '__main__':
+
+    pass
